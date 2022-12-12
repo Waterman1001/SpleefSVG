484cfbd0d4e85d44ad235eb9eaba52ef602fd394
Onno <emailodg@gmail.com>
2022-12-12 17:50:11 +0100

    Hardcoded reward at winevent

--- src/main/java/me/waterman1001/SpleefSVG/listeners/OnPlayerWin.java
+++ src/main/java/me/waterman1001/SpleefSVG/listeners/OnPlayerWin.java
@@ -3,6 +3,7 @@ package me.waterman1001.SpleefSVG.listeners;
 import me.waterman1001.SpleefSVG.listeners.custom.PlayerWinEvent;
 import me.waterman1001.SpleefSVG.utils.Messages;
 import me.waterman1001.SpleefSVG.utils.SpleefPlayerUtils;
+import org.bukkit.Bukkit;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 
@@ -13,6 +14,7 @@ public class OnPlayerWin implements Listener {
 		e.getGame().getGs().broadcastGameMessage(Messages.getInstance().wonTheGame(e.getPlayer()));
 		SpleefPlayerUtils.clearInventory(e.getPlayer());
 		SpleefPlayerUtils.teleport(e.getPlayer(), e.getGame().getMap().getWinLoc());
+		Bukkit.dispatchCommand(Bukkit.getConsoleSender(), "eco give " + e.getPlayer().getName() + " 80");
 		e.getGame().getGs().finishGame();
 	}
 	
